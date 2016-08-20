40db5643715fd1a710f97f97421417518e99c43f
Mitchell Tracy <mittracy7@gmail.com>
2016-08-20 16:32:45 -0400

    updated source

---
+++ src/Node.java
@@ -0,0 +1,35 @@
+public class Node<V> {
+	V value;
+	Node<V> next;
+	Node<V> prev;
+
+	public Node(V value) {
+		this.value = value;
+		this.next = null;
+		this.prev = null;
+	}
+
+	public void setNext(Node<V> next) {
+		this.next = next;
+	}
+
+	public Node<V> getNext() {
+		return this.next;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	public V getValue() {
+		return this.value;
+	}
+
+	public Node<V> getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Node<V> prev) {
+		this.prev = prev;
+	}
+}
--- src/QuickQueue.java
+++ src/QuickQueue.java
@@ -1,8 +1,124 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Queue;
 
-public class QuickQueue {
+public class QuickQueue<E> implements Queue<E> {
+	
+	
+	DoubleLinkedList<E> list;
+	HashMap<E, Node<E>> map;
 	
 	public static void main(String[] args){
 		System.out.println("Hello");
+	}
+
+	@Override
+	public boolean addAll(Collection arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray(Object[] arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E element() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean offer(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E remove() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
